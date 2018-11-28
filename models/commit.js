2601d9e88c97e25006d2aaf5e4927d96d94eec79
youen123 <youlujie123@163.com>
2018-11-28 16:17:06 +0800

    change: 修改exec的方式

--- models/commit.js
+++ models/commit.js
@@ -1,54 +1,67 @@
-const exec = require('child_process').exec
+// const exec = require('child_process').exec
+let { exec } = require('../util/util')
 
 // 获取某个仓库分支的提交
-let getBranchCommits = (localRepoPath, branch, since) => {
-    let cmd = `cd ${localRepoPath} && git checkout ${branch} && git log --pretty=format:"%h #---# %ad #---# %cn #---# %s" --date=iso`
+let getBranchCommits = async (localRepoPath, branch, since) => {
+    let cmd = `cd ${localRepoPath} && git log --pretty=format:"%h #---# %ad #---# %cn #---# %s" --date=iso`
     if (since) {
-        cmd += ` --since=${since}`; 
+        cmd += ` --since=${since}`;
     }
-    return new Promise((resolve, reject) => {
-        exec(cmd, (err, stdout, stderr) => {
-        if(err) {
-            reject(stderr)
-        } else {
-            var arr = stdout.split('\n').filter((item) => {
-            return item !== ''
+    console.log(cmd);
+    try {
+        let preExec = await exec(`cd ${localRepoPath} && git checkout ${branch}`);
+        let ret = await exec(cmd);
+        return {
+            flag: true,
+            res: ret.split('\n').filter((item) => {
+                return item !== ''
             }).map((item) => {
-            let obj = {};
-            let arr = item.split(' #---# ');
-            obj.sha = arr[0];
-            obj.date = arr[1].slice(0, arr[1].length - 6);
-            obj.commiter = arr[2];
-            obj.msg = arr[3];
-            return obj;
-            })
-            console.log(arr)
-            resolve(arr)
-        }
-        })
-    })
-}
-
-  // 获取某个提交的时间
-let getCommitInfo = (localRepoPath, branch) => {
-    let cmd = `cd ${localRepoPath} && git checkout ${branch} && git show --pretty=format:"%h #---# %ad #---# %cn #---# %s" --date=iso`
-    return new Promise((resolve, reject) => {
-        exec(cmd, (err, stdout, stderr) => {
-            if(err) {
-                reject(stderr)
-            } else {
-                var arr = stdout.split('\n');
-                let info = arr[0];
                 let obj = {};
-                arr = info.split(' #---# ');
+                let arr = item.split(' #---# ');
+                console.log(arr);
                 obj.sha = arr[0];
                 obj.date = arr[1].slice(0, arr[1].length - 6);
                 obj.commiter = arr[2];
                 obj.msg = arr[3];
-                resolve(obj);
-            }
-        })
-    })    
+                return obj;
+            })
+        }
+    } catch (err) {
+        console.log(err.message);
+        return {
+            flag: false,
+            errmsg: err.message || 'failed'
+        }
+    }
+}
+
+let getCommitTime = (stdout) => {
+    var arr = stdout.split('\n');
+    let info = arr[0];
+    let obj = {};
+    arr = info.split(' #---# ');
+    obj.sha = arr[0];
+    obj.date = arr[1].slice(0, arr[1].length - 6);
+    obj.commiter = arr[2];
+    obj.msg = arr[3];
+    return obj;
+}
+// 获取某个提交的时间
+let getCommitInfo = async(localRepoPath, branch) => {
+    let cmd = `cd ${localRepoPath} && git checkout ${branch} && git show --pretty=format:"%h #---# %ad #---# %cn #---# %s" --date=iso`
+    console.log(cmd)
+    try {
+        let ret = await exec(cmd);
+        return {
+            flag: true,
+            res: getCommitTime(ret)
+        }
+    } catch (err) {
+        return {
+            flag: false,
+            errmsg: err.message || 'failed'
+        }
+    }
 }
 
 module.exports = {
