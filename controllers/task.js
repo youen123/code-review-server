7445ea0647287067c3f993d8a59edfb10507c14d
youen123 <youlujie123@163.com>
2018-11-28 23:21:01 +0800

    add: 获取行内评论

--- controllers/task.js
+++ controllers/task.js
@@ -35,14 +35,19 @@ module.exports = {
     ctx.response.body = ret.flag ? { flag: true, data: ret.res , msg: '成功'} : ret 
   },
   addComment: async ctx => {
-    let {taskId, type, content} = ctx.request.body
+    let {taskId, type, content, file, commit1, commit2, block_index, line_index} = ctx.request.body
     let creator = ctx.cookies.get('username')
-    const ret = await Comment.addComment({task_id: taskId, type, content, creator})
+    const ret = await Comment.addComment({task_id: taskId, type, content, creator, file, commit1, commit2, block_index, line_index})
     ctx.response.body = ret.flag ? success() : ret
   },
   getCommentsOfTask: async ctx => {
-    let {taskId} = ctx.request.query
-    const ret = await Comment.getCommentsOfTask({task_id: taskId})
+    let {taskId, commit1, commit2, file, type} = ctx.request.query
+    let ret;
+    if (type == 0) {
+      ret = await Comment.getCommentsOfDiff({task_id: taskId, commit1, commit2, file, type})
+    } else {
+      ret = await Comment.getCommentsOfTask({task_id: taskId});
+    }
     ctx.response.body = ret.flag ? success(ret.res) : ret 
   }
 }
