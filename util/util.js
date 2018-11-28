249d97bc5a061e6803b737b2bb0895b3c5664192
youen123 <youlujie123@163.com>
2018-11-28 16:18:30 +0800

    add: 包装exec

--- util/util.js
+++ util/util.js
@@ -1,8 +1,22 @@
+const exec = require('child_process').exec
+
 module.exports = {
     success: (data, msg = '成功') => {
-        return {flag: true, data, msg};
+        return { flag: true, data, msg };
     },
     fail: (errmsg = '失败') => {
-        return {flag: false, data: null, errmsg};
+        return { flag: false, data: null, errmsg };
+    },
+    exec: (cmd) => {
+        return new Promise((resolve, reject) => {
+            exec(cmd, (err, stdout, stderr) => {
+                if (err) {
+                    console.log('error:' + stderr);
+                    reject(stderr)
+                } else {
+                    resolve(stdout)
+                }
+            })
+        });
     }
 }
